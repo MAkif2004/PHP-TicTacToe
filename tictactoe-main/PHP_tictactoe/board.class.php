f5462031f9f916e8d1f3f65f5964c05b14226705
Opariin <83518568+Opariin@users.noreply.github.com>
2022-12-11 22:42:17 +0100

    Add files via upload

---
+++ tictactoe-main/PHP_tictactoe/board.class.php
@@ -0,0 +1,62 @@
+<?php
+
+
+require_once 'enum.php';
+require_once 'index.php';
+require_once 'position.class.php';
+require_once 'player.class.php';
+$position = new Position();
+$player = new Player();
+class Board{
+
+    protected array $board =
+    [
+        [Vakje::empty, Vakje::empty, Vakje::empty,],
+        [Vakje::empty, Vakje::empty, Vakje::empty,],
+        [Vakje::empty, Vakje::empty, Vakje::empty,]
+    ];
+
+
+    public function Show() {
+        foreach ($this->board as $col) {
+            echo '----- ----- -----';
+            echo PHP_EOL;
+            foreach ($col as $row) {
+                switch ($row){
+                    case Vakje::empty:
+                        echo '| . | ';
+                        break;
+                    case Vakje::cross:
+                        echo '| X | ';
+                        break;
+                    case Vakje::circle:
+                        echo '| o | ';
+                        break;
+                    }
+
+            }
+            echo PHP_EOL;
+            echo '----- ----- -----';
+            echo PHP_EOL;
+        }
+    }
+
+
+    public $tries = 0;
+
+    public function Play (int $roww, int $column, $vakje, $tries, $player)
+    {
+        if ($this->board[$roww][$column] == Vakje::empty) {
+            $this->board[$roww][$column] = $vakje;
+
+        } else {
+            $this->tries += 1;
+            $player->switchTeam();
+        }
+    }
+
+}
+
+
+
+?>
